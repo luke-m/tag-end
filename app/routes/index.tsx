4197c93754fd34287d8919f34c4c4c8127f08937
Lukem <lukasm2411@gmail.com>
2022-01-13 23:19:02 +0100

    init to win it

---
+++ app/routes/index.tsx
@@ -0,0 +1,15 @@
+export default function Index() {
+
+  // const requestUserAccessToken = () => {
+  //   window.location.href = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(window.location.href)}&scope=playlist-modify-public`;
+  // }
+
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Tag-End</h1>
+      <h2>Create dynamic Spotify playlists based on tags!</h2>
+      
+      <button onClick={}>Click here to log in to Spotify</button>
+    </div>
+  );
+}
